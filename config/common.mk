8da3870abf24f976e9e06c99598ab7decab1ed90
jgcaap <jgcaap@gmail.com>
2015-05-12 23:30:56 -0400

    Merge branch 'cm-12.1' of https://github.com/CyanogenMod/android_vendor_cm into cm-12.1
    Change-Id: I2da76e9684ad47f31c5c85b1fe208a9d59e4c1e0

--- config/common.mk
+++ config/common.mk
@@ -144,6 +144,14 @@ PRODUCT_PACKAGES += \
     CyanogenSetupWizard \
     CameraNext
 
+ifeq ($(filter NIGHTLY SNAPSHOT EXPERIMENTAL,$(CM_BUILDTYPE)),)
+# CM Platform Library
+PRODUCT_PACKAGES += \
+    org.cyanogenmod.platform-res \
+    org.cyanogenmod.platform \
+    org.cyanogenmod.platform.xml
+endif
+
 # CM Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
     org.cyanogenmod.hardware \
